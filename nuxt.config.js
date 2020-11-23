f7586fed65d5fa09e925061b11b4d7f626a99825
Kira2k <judoshark@gmail.com>
2020-11-23 14:17:47 +0300

    minor bug fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: "~/plugins/Vuelidate", ssr: false }],
+  plugins: [{ src: "~/plugins/Vuelidate" }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
